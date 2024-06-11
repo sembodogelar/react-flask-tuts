143fda952c5e09128d54169bae89cd1079554c16
SembodoGelar <sembodogelar6@gamail.com>
2024-06-11 16:37:06 +0700

    first commit

---
+++ backend/models.py
@@ -0,0 +1,20 @@
+from app import db # accessing 'db' variable on app.py
+
+class Friend(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    img_url = db.Column(db.String(200), nullable=True)
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "role":self.role,
+            "description":self.description,
+            "gender":self.gender,
+            "imgUrl":self.img_url,
+
+        }
---
+++ backend/routes.py
@@ -0,0 +1,6 @@
+print("this is my db file")
+
+db = 10
+
+if __name__ == "__main__":
+    print("This file is called directly")
