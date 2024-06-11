8351882a2e7c5455614e3b978e4595f77b937570
SembodoGelar <sembodogelar6@gamail.com>
2024-06-11 19:08:04 +0700

    after api test with postman

--- backend/routes.py
+++ backend/routes.py
@@ -8,3 +8,34 @@ def get_friends():
     friends = Friend.query.all()
     result = [friend.to_json() for friend in friends]
     return jsonify(result)
+
+# Create a friend
+@app.route("/api/friends",methods=["POST"])
+def create_friend():
+    try:
+        data = request.json
+
+        name = data.get("name")
+        role = data.get("role")
+        description = data.get("description")
+        gender = data.get("gender")
+
+       #fetch avatar image base on gender
+        if gender == "male":
+            img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+        elif gender == "female":
+            img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+        else:
+            img_url = None
+
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+
+        db.session.add(new_friend)
+            
+        db.session.commit()
+
+        return jsonify({"msg":"friend created success"}), 201
+        
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":str(e)}), 500
